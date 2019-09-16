b0f9bfbbc65b4daf7eb19ecb6366530fba823f5e
kosengro <goseungro@naver.com>
2019-09-16 09:07:00 +0900

    9_16 이지현 빨리와

---
+++ Nobrains/Assets/02.Scripts/New Folder/Billboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Billboard : MonoBehaviour
+{
+    private Transform camTr;
+    private Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTr = GameObject.Find("[CameraRig]/Camera").GetComponent<Transform>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.LookAt(camTr.position);
+    }
+}
---
+++ Nobrains/Assets/02.Scripts/New Folder/GoBrain.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class GoBrain : MonoBehaviour
+{
+    private Transform tr;
+    private Animator anim;
+    private int hashOpen;
+    private bool opening = false;
+    
+    public SteamVR_Input_Sources hand = SteamVR_Input_Sources.Any;
+
+    private SteamVR_Action_Boolean grab = SteamVR_Actions.default_GrabGrip;
+    private SteamVR_Action_Boolean trigger = SteamVR_Actions.default_InteractUI;
+    private SteamVR_Action_Vibration haptic = SteamVR_Actions.default_Haptic;
+
+    public float rotSpeed = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        hashOpen = Animator.StringToHash("BrainOpening");               
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.RotateAround(tr.position, Vector3.up, Time.deltaTime * rotSpeed);
+        if (grab.GetStateDown(hand))
+        {
+            BrainOpening();
+        }
+
+    }
+    void BrainOpening()
+    {        
+        //한 버튼으로 할 수 없는지 질문!        
+        if (!opening)
+        {
+            Debug.Log("Open!");
+            haptic.Execute(0.2f, 0.3f, 10f, 0.5f, hand);
+            anim.SetTrigger("BrainOpen");
+            opening = true;           
+            
+        }        
+            if (opening)
+            {
+                Debug.Log("Close!");
+                haptic.Execute(0.2f, 0.3f, 10f, 0.5f, hand);
+                anim.SetTrigger("BrainOpen");
+                opening = false;              
+                
+            }
+        
+    }
+   
+}
