b2aad1c81062ac3b690720c9890c610881b2d2ee
kosengro <goseungro@naver.com>
2019-09-17 15:47:13 +0900

    9_17 Brain UI

--- Nobrains/Assets/02.Scripts/New Folder/GoBrain.cs
+++ Nobrains/Assets/02.Scripts/New Folder/GoBrain.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using UnityEngine.EventSystems;
 
 public class GoBrain : MonoBehaviour
 {
@@ -9,23 +10,23 @@ public class GoBrain : MonoBehaviour
     private Animator anim;
     private int hashOpen;
     private bool opening = false;
-    
+
     public SteamVR_Input_Sources hand = SteamVR_Input_Sources.Any;
 
     private SteamVR_Action_Boolean grab = SteamVR_Actions.default_GrabGrip;
     private SteamVR_Action_Boolean trigger = SteamVR_Actions.default_InteractUI;
     private SteamVR_Action_Vibration haptic = SteamVR_Actions.default_Haptic;
 
-    public float rotSpeed = 15f;
-    // Start is called before the first frame update
+    public float rotSpeed = 15f;   
+    
     void Start()
     {
         tr = GetComponent<Transform>();
         anim = GetComponent<Animator>();
-        hashOpen = Animator.StringToHash("BrainOpening");               
+        hashOpen = Animator.StringToHash("BrainOpening");        
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         tr.RotateAround(tr.position, Vector3.up, Time.deltaTime * rotSpeed);
@@ -36,25 +37,23 @@ public class GoBrain : MonoBehaviour
 
     }
     void BrainOpening()
-    {        
-        //한 버튼으로 할 수 없는지 질문!        
+    {
+                
         if (!opening)
         {
             Debug.Log("Open!");
             haptic.Execute(0.2f, 0.3f, 10f, 0.5f, hand);
             anim.SetTrigger("BrainOpen");
-            opening = true;           
-            
-        }        
-            if (opening)
-            {
-                Debug.Log("Close!");
-                haptic.Execute(0.2f, 0.3f, 10f, 0.5f, hand);
-                anim.SetTrigger("BrainOpen");
-                opening = false;              
-                
-            }
-        
-    }
-   
+            opening = true;
+        }
+        if (opening)
+        {
+            Debug.Log("Close!");
+            haptic.Execute(0.2f, 0.3f, 10f, 0.5f, hand);
+            anim.SetTrigger("BrainOpen");
+            opening = false;
+        }
+
+    }   
+
 }
--- Nobrains/Assets/02.Scripts/New Folder/LaserPointer.cs
+++ Nobrains/Assets/02.Scripts/New Folder/LaserPointer.cs
@@ -12,19 +12,33 @@ public class LaserPointer : MonoBehaviour
     private LineRenderer line;
 
     private SteamVR_Action_Boolean trigger = SteamVR_Actions.default_InteractUI;
-    
+
     public float range = 20f;
     // public Color defaultColor = Color.green;
 
     private RaycastHit hit;
     private Ray ray;
+    private int brainLayer;
+    private GameObject currBrain;
+    private GameObject prevBrain;
+    private bool uiClick = false;
+
+    public delegate void LaserEnterHandler(GameObject cBrain);
+    public static event LaserEnterHandler OnLaserEnter;
+
+    // public delegate void LaserClickHandler();
+    // public static event LaserClickHandler OnLaserClick;
+
+    public delegate void LaserExitHandler(GameObject pBrain);
+    public static event LaserExitHandler OnLaserExit;
 
     void Start()
     {
         pose = GetComponent<SteamVR_Behaviour_Pose>();
         hand = pose.inputSource;
         tr = GetComponent<Transform>();
-        
+        brainLayer = 1 << LayerMask.NameToLayer("BRAIN");
+
         CreateLine();
 
     }
@@ -32,6 +46,21 @@ public class LaserPointer : MonoBehaviour
     void Update()
     {
         ray = new Ray(tr.position, tr.forward);
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            line.SetPosition(1, new Vector3(0, 0, hit.distance));
+        }
+        if (!Physics.Raycast(ray, out hit, range))
+        {
+            line.SetPosition(1, new Vector3(0, 0, range));
+        }
+
+        BrainPart();
+
+        if (trigger.GetStateDown(hand))
+        {
+            ClickUI();
+        }
 
     }
     void CreateLine()
@@ -41,14 +70,51 @@ public class LaserPointer : MonoBehaviour
         line.receiveShadows = false;
         line.positionCount = 2;
         line.SetPosition(0, Vector3.zero);
-        line.SetPosition(1, new Vector3(0,0,range));
+        line.SetPosition(1, new Vector3(0, 0, range));
         line.startColor = Color.green;
         line.endColor = Color.red;
-        line.startWidth = 0.01f;
+        line.startWidth = 0.005f;
         line.endWidth = 0.005f;
         line.material = new Material(Shader.Find("Legacy Shaders/Particles/Additive"));
         // line.material.color = defaultColor;
 
     }
-    
+    void BrainPart()
+    {
+        if (Physics.Raycast(ray, out hit, range, brainLayer))
+        {
+            currBrain = hit.collider.gameObject;
+            if (currBrain != prevBrain) //현재 선택한 뇌의 부분과 이전에 선택했던 뇌의 부분이 다른 경우
+            {
+                OnLaserEnter(currBrain); //현재 선택한 뇌의 부분에 OnLaserEnter이벤트 전달
+                OnLaserExit(prevBrain); //이전에 선택했던 뇌의 부분에 OnLaserExit이벤트 전달                
+                prevBrain = currBrain; //currBrain이 prevBrain이 되도록                
+            }
+        }
+        else
+        {
+            ReleaseBrain(); //레이저가 뇌 이외에 다른 부분을 선택했을 경우 OnLaserExit이벤트 전달
+        }
+    }
+    void ReleaseBrain()
+    {
+        if (prevBrain != null) //이전에 선택헀던 뇌의 부분이 있을 경우
+        {
+            OnLaserExit(prevBrain); //이전에 선택했던 뇌의 부분에 OnLaserExit이벤트 전달                       
+            prevBrain = null; //이전의 뇌의 부분을 초기화
+        }
+    }
+    void ClickUI()
+    {
+        if (!uiClick && Physics.Raycast(ray, out hit, range, brainLayer))
+        {
+            hit.collider.gameObject.transform.Find("Canvas").gameObject.SetActive(true);
+            uiClick = true;            
+        }
+        else 
+        {
+            hit.collider.gameObject.transform.Find("Canvas").gameObject.SetActive(false);
+            uiClick = false;            
+        }
+    }
 }
