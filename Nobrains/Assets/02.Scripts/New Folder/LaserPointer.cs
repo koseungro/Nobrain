5913779b1ec263967e4f86e86014ca8e8dab1c7e
kosengro <goseungro@naver.com>
2019-09-19 08:44:04 +0900

    9_19 뇌 UI

--- Nobrains/Assets/02.Scripts/New Folder/LaserPointer.cs
+++ Nobrains/Assets/02.Scripts/New Folder/LaserPointer.cs
@@ -10,6 +10,7 @@ public class LaserPointer : MonoBehaviour
     private SteamVR_Input_Sources hand;
     private Transform tr;
     private LineRenderer line;
+    private GameObject pointer;
 
     private SteamVR_Action_Boolean trigger = SteamVR_Actions.default_InteractUI;
 
@@ -39,21 +40,16 @@ public class LaserPointer : MonoBehaviour
         tr = GetComponent<Transform>();
         brainLayer = 1 << LayerMask.NameToLayer("BRAIN");
 
+        GameObject _pointer = Resources.Load<GameObject>("Pointer");
+        pointer = Instantiate(_pointer);
+
         CreateLine();
 
     }
 
     void Update()
     {
-        ray = new Ray(tr.position, tr.forward);
-        if (Physics.Raycast(ray, out hit, range))
-        {
-            line.SetPosition(1, new Vector3(0, 0, hit.distance));
-        }
-        if (!Physics.Raycast(ray, out hit, range))
-        {
-            line.SetPosition(1, new Vector3(0, 0, range));
-        }
+        Pointer();
 
         BrainPart();
 
@@ -78,6 +74,34 @@ public class LaserPointer : MonoBehaviour
         line.material = new Material(Shader.Find("Legacy Shaders/Particles/Additive"));
         // line.material.color = defaultColor;
 
+    }
+    void Pointer()
+    {
+        ray = new Ray(tr.position, tr.forward);
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            line.SetPosition(1, new Vector3(0, 0, hit.distance)); //라인이 오브젝트에 닿으면 그 hit을 끝점으로
+            if (hit.collider.gameObject.layer == 8) //만약 라인이 뇌에 닿으면 포인터 생성
+            {
+                pointer.transform.gameObject.SetActive(true);
+                pointer.transform.position = hit.point;
+                pointer.transform.rotation = Quaternion.LookRotation(hit.normal); //포인터의 각도 조정
+                
+            }            
+            else //라인이 뇌가 아닌 오브젝트에 닿으면 포인터 비활성화
+            {
+                pointer.transform.gameObject.SetActive(false);
+            }
+        }
+        else //라인이 오브젝트에 닿지 않으면 길이 그대로, 포인터 비활성화
+        {
+            line.SetPosition(1, new Vector3(0, 0, range));
+            pointer.transform.gameObject.SetActive(false);
+        }
+        // if (!Physics.Raycast(ray, out hit, range))
+        // {
+        //     line.SetPosition(1, new Vector3(0, 0, range));
+        // }
     }
     void BrainPart()
     {
@@ -109,12 +133,12 @@ public class LaserPointer : MonoBehaviour
         if (!uiClick && Physics.Raycast(ray, out hit, range, brainLayer))
         {
             hit.collider.gameObject.transform.Find("Canvas").gameObject.SetActive(true);
-            uiClick = true;            
+            uiClick = true;
         }
-        else 
+        else
         {
             hit.collider.gameObject.transform.Find("Canvas").gameObject.SetActive(false);
-            uiClick = false;            
+            uiClick = false;
         }
     }
 }
