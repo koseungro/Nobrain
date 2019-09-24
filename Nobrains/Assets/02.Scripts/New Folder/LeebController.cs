41397a976a230be07b95f214603834e441971693
kosengro <goseungro@naver.com>
2019-09-24 18:14:08 +0900

    9_24 Test

--- Nobrains/Assets/02.Scripts/New Folder/LeebController.cs
+++ Nobrains/Assets/02.Scripts/New Folder/LeebController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Valve.VR;
 using UnityEngine.EventSystems;
 
-public class GobController : MonoBehaviour
+public class LeebController : MonoBehaviour
 {
     private Animator anim;
     private GameObject selectedBrain;
@@ -12,10 +12,14 @@ public class GobController : MonoBehaviour
     private void OnEnable()
     {
         LaserPointer.OnLaserEnter += LaserEnter;
+        //LaserPointer.OnLaserClick += LaserClick;
+
     }
     private void OnDisable()
     {
         LaserPointer.OnLaserEnter -= LaserEnter;
+        //LaserPointer.OnLaserClick -= LaserClick;
+
     }
 
     void Start()
@@ -23,51 +27,28 @@ public class GobController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    void PlayAnim()
+    public void PlayAnim()
     {
         //애니메이션 실행
         switch (selectedBrain.tag)
-        {
+        {            
             case "frontal":
                 anim.SetTrigger("hash_frontal");
-                break;
-            case "parietal":
-                anim.SetTrigger("hash_parietal");
-                break;
+                break;            
             case "temporal":
                 anim.SetTrigger("hash_temporal");
                 break;
             case "occipital":
                 anim.SetTrigger("hash_occipital");
-                break;
-            case "Broca":
-                anim.SetTrigger("hash_Broca");
-                break;
-            case "Wernicke":
-                anim.SetTrigger("hash_Wernicke");
-                break;
-            case "diencephaion":
-                anim.SetTrigger("hash_diencephaion");
-                break;
-            case "stem":
-                anim.SetTrigger("hash_stem");
-                break;
-            case "cerebellum":
-                anim.SetTrigger("hash_cerebellum");
-                break;
+                break;            
         }
-
     }
 
     void LaserEnter(GameObject brain)
     {
         selectedBrain = brain;
     }
+    
 
 
 }
